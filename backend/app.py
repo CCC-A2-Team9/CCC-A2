1e4f16c3379b9a666007b4c7c6bb8102a3ad7e2a
Shimzheng <80332524+Shimzheng@users.noreply.github.com>
2022-05-09 12:14:25 +1000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -296,7 +296,7 @@ def set3():
 
 if __name__ == '__main__':
     server = couchdb.Server("http://admin:admin@172.26.132.76:5984")
-    db1 = server['scenario1']
+    db1 = server['testscenario4']
     db2 = server['scenario2']
     db3 = server['scenario3']
     aurin1 = server['aurin1']
